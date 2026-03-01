d75c64aaa4517c5486c54da0c97aa767d0e19448
nekramin <r0m41k041@gmail.com>
2026-03-01 15:45:51 +0300

    da

---
+++ Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManagerHome
+{
+    public class Task : Program
+    {
+        public static void ShowInfo(string Title, string Description, bool isDone, string Deadline)
+        {
+            Console.Write($"Имя задачи: {Title}\nОписание задачи: {Description}\nСтатус задачи: ");
+            checkStatus(isDone);
+            Console.Write($"Дедлайн: ");
+            WorkTask.ShowDeadline(Deadline);
+        }
+    }
+}
